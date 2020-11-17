f4ba144be8c7606cb6f5bfbeca7cdb8ceb03204d
onyedikachi-david <51977119+onyedikachi-david@users.noreply.github.com>
2020-11-17 15:54:35 +0100

    My craigelist clone

---
+++ kachilist/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Search
+
+# Register your models here.
+# LOGIN CREDENTIALS
+# USER NAME = MYAPP
+# PASSWORD = MYAPP
+
+admin.site.register(Search)
