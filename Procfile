e1c7e7e72741a29892f6de5253198587bc58a1b1
onyedikachi-david <51977119+onyedikachi-david@users.noreply.github.com>
2020-11-28 15:59:44 +0100

    Kinda done with this

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn crasiglist_app.wsgi
--- kachilist/views.py
+++ kachilist/views.py
@@ -1,9 +1,14 @@
+# from urllib.parse import quote_plus
+from requests.compat import quote_plus
+from . import models
 from django.shortcuts import render
-import request
+import requests
 from bs4 import BeautifulSoup
 
-
 # Create your views here.
+BASE_URL_CRAIGELIST = 'https://newyork.craigslist.org/d/services/search/?query={}'
+BASE_IMAGE_URL = 'http://images.craigelist.org/_{}_300x300.jpg'
+
 
 def home(request):
     return render(request, 'base.html')
@@ -11,8 +16,40 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
-    print(search)
+    models.Search.objects.create(search=search)
+    # print(quote_plus(search))
+    final_url = BASE_URL_CRAIGELIST.format(quote_plus(search))
+    print(final_url)
+    response = requests.get(final_url)
+    data = response.text
+
+    soup = BeautifulSoup(data, features='html.parser')
+
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+        post_text = post.find(id='postingbody').text
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+        else:
+            post_image_url = 'https://craigelist.org/images/peace.jpg'
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+
+    # print(data)
+    # print(search)
     stuff_for_front_end = {
         'search': search,
+        'final_postings': final_postings,
     }
     return render(request, 'kachilist/new_search.html', stuff_for_front_end)
